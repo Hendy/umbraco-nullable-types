916133d6dd91d9a065c8ee70f02b4b328225bb6e
Hendy Racher <git@hendyracher.co.uk>
2018-06-16 16:53:18 +0100

    property editor definitions for bool?

---
+++ src/Our.Umbraco.NullableTypes/PropertyEditors/NullableBooleanPreValueEditor.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.NullableTypes.PropertyEditors
+{
+    internal class NullableBooleanPreValueEditor : PreValueEditor
+    {
+        [PreValueField("nullLabel", "Null Label", "textstring", Description = "")]
+        public string NullLabel { get; set; }
+
+        [PreValueField("falseLabel", "False Label", "textstring", Description = "")]
+        public string FalseLabel { get; set; }
+
+        [PreValueField("trueLabel", "True Label", "textstring", Description = "")]
+        public string TrueLabel { get; set; }
+    }
+}
---
+++ src/Our.Umbraco.NullableTypes/PropertyEditors/NullableBooleanPropertyEditor.cs
@@ -0,0 +1,30 @@
+﻿using ClientDependency.Core;
+using System.Collections.Generic;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web.PropertyEditors;
+
+namespace Our.Umbraco.NullableTypes.PropertyEditors
+{
+    [PropertyEditor(PropertyEditorAlias, "Nullable Boolean", "~/App_Plugins/NullableBoolean/NullableBoolean.html", ValueType = "JSON")]
+    [PropertyEditorAsset(ClientDependencyType.Css, "~/App_Plugins/NullableBoolean/NullableBoolean.css")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "~/App_Plugins/NullableBoolean/NullableBoolean.js")]
+    public class NullableBooleanPropertyEditor : PropertyEditor
+    {
+        public const string PropertyEditorAlias = "Our.Umbraco.NullableBoolean";
+
+        public NullableBooleanPropertyEditor() : base()
+        {
+            this.DefaultPreValues = new Dictionary<string, object>
+            {
+                { "nullLabel", "Null" },
+                { "falseLabel", "False" },
+                { "trueLabel", "True" }
+            };
+        }
+
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new NullableBooleanPreValueEditor();
+        }
+    }
+}
