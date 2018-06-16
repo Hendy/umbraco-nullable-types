2dbdebf79f3581a387955f8f7a3012242e45d98e
Hendy Racher <git@hendyracher.co.uk>
2018-06-16 16:43:42 +0100

    added NullableBoolean resource files

---
+++ src/Our.Umbraco.NullableTypes/Resources/NullableBoolean.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    'use strict';
+
+    angular
+        .module("umbraco")
+        .controller("NullableBooleanController", NullableBooleanController);
+
+    NullableBooleanController.$inject = ['$scope'];
+
+    function NullableBooleanController($scope) {
+
+        $scope.model.value = $scope.model.value || '';
+
+    }
+
+})();
