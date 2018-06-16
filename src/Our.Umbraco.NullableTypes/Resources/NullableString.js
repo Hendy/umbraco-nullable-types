f460253255334f7e35924f411c0265f9728e812f
Hendy Racher <git@hendyracher.co.uk>
2018-06-16 17:04:19 +0100

    resource files for nullable string

---
+++ src/Our.Umbraco.NullableTypes/Resources/NullableString.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    'use strict';
+
+    angular
+        .module("umbraco")
+        .controller("NullableStringController", NullableStringController);
+
+    NullableStringController.$inject = ['$scope'];
+
+    function NullableStringController($scope) {
+
+
+    }
+
+})();
