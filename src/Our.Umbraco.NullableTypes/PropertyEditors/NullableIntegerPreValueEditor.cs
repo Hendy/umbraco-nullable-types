b99cef39455f1020bc8444a9c8216aa3f40db493
Hendy Racher <git@hendyracher.co.uk>
2018-06-18 12:34:16 +0100

    nullable integer functional

--- src/Our.Umbraco.NullableTypes/PropertyEditors/NullableIntegerPreValueEditor.cs
+++ src/Our.Umbraco.NullableTypes/PropertyEditors/NullableIntegerPreValueEditor.cs
@@ -4,5 +4,18 @@ namespace Our.Umbraco.NullableTypes.PropertyEditors
 {
     internal class NullableIntegerPreValueEditor : PreValueEditor
     {
+        [PreValueField(
+            "defaultCheckboxState",
+            "Default Checkbox State",
+            "boolean",
+            Description = "When checked, the number field is initially active (non null mode)")]
+        public bool DefaultCheckboxState { get; set; }
+
+        [PreValueField(
+            "hideValueWhenNull",
+            "Hide Value When Null",
+            "boolean",
+            Description = "When checked, any value in the number field will be hidden in null mode")]
+        public bool HideValueWhenNull { get; set; }
     }
 }
--- src/Our.Umbraco.NullableTypes/Resources/NullableInteger.js
+++ src/Our.Umbraco.NullableTypes/Resources/NullableInteger.js
@@ -10,8 +10,42 @@
 
     function NullableIntegerController($scope) {
 
-        $scope.model.value = $scope.model.value || '';
+        var config = {
+            defaultCheckboxState: $scope.model.config.defaultCheckboxState == '1',
+            hideValueWhenNull: $scope.model.config.hideValueWhenNull == '1'
+        };
 
+        var notNull = function () { return $scope.model.value.checkbox; };
+
+        // init value
+        if (typeof $scope.model.value != 'object') { // handle non-standard save formats
+            $scope.model.value = {
+                checkbox: config.defaultCheckboxState,
+                number: $scope.model.value // expecting null or integer (but this could be any value if migrating away from another data-type)
+            };
+        }
+
+        // set working copy for the number field
+        if (!config.hideValueWhenNull || notNull()) {
+            $scope.model.number = $scope.model.value.number;
+        }
+
+        $scope.checkboxChange = function () {
+            if (config.hideValueWhenNull) {
+                if (notNull()) {
+                    $scope.model.number = $scope.model.value.number;
+                } else {
+                    $scope.model.value.number = $scope.model.number;
+                    $scope.model.number = '';
+                }
+            }
+        };
+
+        $scope.$on("formSubmitting", function () {
+            if (!config.hideValueWhenNull || notNull()) {
+                $scope.model.value.number = $scope.model.number;
+            }
+        });
     }
 
 })();
